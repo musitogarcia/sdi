424c88268587dc4e2fbb077ece3212cea9b12ba0
musitogarcia <46464181+musitogarcia@users.noreply.github.com>
2021-03-13 09:42:59 -0600

    antes de pdf

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -7,13 +7,4 @@ $this->assign('title', 'Inicio');
         <?= $this->Identity->get('username') ?>
     </small>
 </h1>
-<h1 class="display-6">
-    Usuario
-    <small class="text-muted">
-        <?= $this->Identity->get('role')['descripcion'] ?>
-    </small>
-</h1>
-<p class="lead">
-    Hora de ingreso:
-    <?= date('H:i') ?>
-</p>
+<div class="col-mb-5"></div>
